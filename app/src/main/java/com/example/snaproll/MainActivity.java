2e94bc264a0ce606d47d9229d43afb0d5cd35356
JoshM0311 <jmondragon0311@gmail.com>
2024-02-28 14:01:38 -0600

    first commit

---
+++ app/src/main/java/com/example/snaproll/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.snaproll;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navAttendance) {
+                    loadFragment(new AttendanceFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.navNotes) {
+                    loadFragment(new NotesFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.navProfile) {
+                    loadFragment(new ProfileFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Display the navAttendance upon opening the app
+        loadFragment(new AttendanceFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
